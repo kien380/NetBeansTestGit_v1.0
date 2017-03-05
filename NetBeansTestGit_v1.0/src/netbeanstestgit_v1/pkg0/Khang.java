cb26f6e1d04625261dc9810544db390f10a5d76a
Khang <victorho741953@gmail.com>
2017-03-05 11:04:05 +0700

    Khang_v1.1.0

---
+++ NetBeansTestGit_v1.0/src/netbeanstestgit_v1/pkg0/Khang.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package netbeanstestgit_v1.pkg0;
+
+/**
+ *
+ * @author Ho Khang
+ */
+public class Khang {
+    public static double t = 1996;
+    public static String thanh = "retarded";
+}
