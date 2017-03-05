9863ce0bb81db57f775add4845b1c48cc7d67a86
công <conghua2411@gmail.com>
2017-03-05 11:04:12 +0700

    fuck

---
+++ NetBeansTestGit_v1.0/src/netbeanstestgit_v1/pkg0/quanque.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package netbeanstestgit_v1.pkg0;
+
+/**
+ *
+ * @author CongHua
+ */
+public class quanque {
+    public static int thanhPro = 69;
+    
+    public static String khang = "co ho";
+}
