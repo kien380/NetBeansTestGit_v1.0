504e095b6751f2381db424084a8c113bbd870ed4
KienHHT <huynhhoatrungkien@gmail.com>
2017-03-05 10:53:50 +0700

    init

---
+++ NetBeansTestGit_v1.0/src/netbeanstestgit_v1/pkg0/NetBeansTestGit_v10.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package netbeanstestgit_v1.pkg0;
+
+/**
+ *
+ * @author Ken Huynh
+ */
+public class NetBeansTestGit_v10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hế lô mí cưng <3");
+    }
+    
+}
